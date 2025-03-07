6c046471632f73f4622b6c655e583e7a9d017350
Reiaw <160653577+Reiaw@users.noreply.github.com>
2025-03-07 15:39:49 +0700

    Delete myprojects directory

--- myprojects/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from myapp import views
-
-urlpatterns = [
-   path('',views.index),
-   path('form',views.form),
-   path('edit/<person_id>',views.edit),
-   path('delete/<person_id>',views.delete)
-]
